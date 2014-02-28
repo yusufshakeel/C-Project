e64c8935baa26e0a9f350796c1844120244a6bc2
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-02-28 20:58:50 +0530

    Initial commit

---
+++ c/forloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	//declaring variables
+	int i,n;
+
+	//reading value of n
+	scanf("%d",&n);
+
+	//for loop
+	for(i=1;i<=n;i++){
+		printf("%d\n",i);
+	}
+	return 0;
+}
