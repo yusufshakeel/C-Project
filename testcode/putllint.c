b1b92026bbe30acdc6332bb9a1599494c344dff9
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-07-10 11:46:46 +0530

    for long long int value

---
+++ testcode/putllint.c
@@ -0,0 +1,36 @@
+/*for long long int
+*/
+#include <stdio.h>
+#define _pc(x) putchar_unlocked(x);
+
+inline void putllint(long long int n){
+	long long int num = n, rev = n, c = 0;
+	if(num == 0){
+		_pc('0');
+		_pc('\n');
+		return;
+	}
+	while(rev % 10 == 0){
+		c++;
+		rev /= 10;
+	}
+	rev = 0;
+	while(num != 0){
+		rev = (rev << 3) + (rev << 1) + num % 10;
+		num /= 10;
+	}
+	while(rev != 0){
+		_pc(rev % 10 + '0');
+		rev /= 10;
+	}
+	while(c--){
+		_pc('0');
+	}
+	_pc('\n');
+}
+
+int main(){
+	long long int n = 123456;
+	putllint(n);
+	return 0;
+}
