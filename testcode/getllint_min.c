3d9751757c09a75e1ecf02b46d9ffa3be298f22c
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-07-11 00:16:59 +0530

    minified

--- testcode/getllint_min.c
+++ testcode/getllint_min.c
@@ -4,12 +4,7 @@
 #include <stdio.h>
 #define _gc() getchar_unlocked()
 
-inline long long int getllint(){
-long long int x=0,c=_gc();
-while(!(c>='0'&&c<='9')){c=_gc();}
-while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}
-return x;
-}
+inline long long int getllint(){long long int x=0,c=_gc();while(!(c>='0'&&c<='9')){c=_gc();}while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}return x;}
 
 int main(){
 	long long int x = getllint();
