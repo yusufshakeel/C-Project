cef12088f52a9911989866b4616138c7e1c748a5
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-07-10 15:17:12 +0530

    moved to DY-Project directory

--- testdata/in_data_gen.py
+++
@@ -1,9 +0,0 @@
-#author: yusuf shakeel
-#date: 09-july-2014 wednesday
-
-filename = "in10.dat"
-noOfByte = 10
-myfile = open(filename,'w')
-for i in range(noOfByte):
-	myfile.write("0")
-myfile.close()
