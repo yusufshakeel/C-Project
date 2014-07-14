855564b90713aeeeca6905c253697dfa6d16a723
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-07-14 20:49:41 +0530

    get and put integer

--- testcode/io.c
+++ testcode/io.c
@@ -12,3 +12,8 @@ inline void putlint(long int n){long int N=n,r=n,c=0;if(N==0){_pc('0');_pc('\n')
 /* long long int */
 inline long long int getllint(){long long int x=0,c=_gc();while(!(c>='0'&&c<='9')){c=_gc();}while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}return x;}
 inline void putllint(long long int n){long long int N=n,r=n,c=0;if(N==0){_pc('0');_pc('\n');return;}while(r%10==0){c++;r/=10;}r=0;while(N!=0){r=(r<<3)+(r<<1)+N%10;N/=10;}while(r!=0){_pc(r%10+'0');r/=10;}while(c--){_pc('0');}_pc('\n');}
+
+/* template */
+typedef long long int iTYPE;
+inline iTYPE getint(){iTYPE x=0,c=_gc();while(!(c>='0'&&c<='9')){c=_gc();}while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}return x;}
+inline void putint(iTYPE n){iTYPE N=n,r=n,c=0;if(N==0){_pc('0');_pc('\n');return;}while(r%10==0){c++;r/=10;}r=0;while(N!=0){r=(r<<3)+(r<<1)+N%10;N/=10;}while(r!=0){_pc(r%10+'0');r/=10;}while(c--){_pc('0');}_pc('\n');}
