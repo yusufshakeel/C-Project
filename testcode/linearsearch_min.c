7b415a3f6e699bd4414acea186f570a4fd2fe3f4
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-07-14 11:38:13 +0530

    linear search

--- testcode/linearsearch_min.c
+++ testcode/linearsearch_min.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-inline int lsrch(int *a,int n,int k){int i;for(i=0;i<n;i++){if(*(a+i)==k)return i;}return -1;}
+inline int lsrchint(int *a,int n,int k){int i;for(i=0;i<n;i++){if(*(a+i)==k)return i;}return -1;}
 
 inline long int lsrchlint(long int *a,long int n,long int k){long int i;for(i=0;i<n;i++){if(*(a+i)==k)return i;}return -1;}
 
@@ -8,6 +8,6 @@ inline long long int lsrchllint(long long int *a,long long int n,long long int k
 
 int main(){
 	int arr[], arr_size, elem;
-	int index = lsrch(arr,arr_size,elem);
+	int index = lsrchint(arr,arr_size,elem);
 	return 0;
 }
