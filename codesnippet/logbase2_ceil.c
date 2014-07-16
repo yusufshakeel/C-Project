9e82ee0d157e6e4c03fa061e33d0f3b6bb9796fd
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-07-16 11:50:33 +0530

    log base 2 ceil

---
+++ codesnippet/logbase2_ceil.c
@@ -0,0 +1,35 @@
+int ceil_log2(unsigned long long x)
+{
+  static const unsigned long long t[6] = {
+    0xFFFFFFFF00000000ull,
+    0x00000000FFFF0000ull,
+    0x000000000000FF00ull,
+    0x00000000000000F0ull,
+    0x000000000000000Cull,
+    0x0000000000000002ull
+  };
+
+  int y = (((x & (x - 1)) == 0) ? 0 : 1);
+  int j = 32;
+  int i;
+
+  for (i = 0; i < 6; i++) {
+    int k = (((x & t[i]) == 0) ? 0 : j);
+    y += k;
+    x >>= k;
+    j >>= 1;
+  }
+
+  return y;
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("%d\n", ceil_log2(atol(argv[1])));
+
+  return 0;
+}
