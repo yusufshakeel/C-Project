b6e778b475f7541242da9dfadd3b326b80a61c7c
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-07-16 11:49:28 +0530

    minified

---
+++ codesnippet/getint_min.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define _gc() getchar_unlocked()
+
+inline int getint(){int x=0;register int c=_gc();while(!(c>='0'&&c<='9')){c=_gc();}while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}return x;}
+
+int main(){
+	int x = getint();
+	printf("%d\n", x);
+	return 0;
+}
