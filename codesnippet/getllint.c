c5a766499173dfb13e51073bf55864eff0439dc8
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-07-16 11:49:40 +0530

    get long long int

---
+++ codesnippet/getllint.c
@@ -0,0 +1,23 @@
+/*for integer
+*/
+
+#include <stdio.h>
+#define _gc() getchar_unlocked()
+
+inline long long int getllint(){
+	long long int x = 0, c = _gc();
+	while(!(c >= '0' && c <= '9')){
+		c = _gc();
+	}
+	while(c >= '0' && c <= '9'){
+		x = (x << 3) + (x << 1) + (c - '0');
+		c = _gc();
+	}
+	return x;
+}
+
+int main(){
+	long long int x = getllint();
+	printf("%lld\n", x);
+	return 0;
+}
