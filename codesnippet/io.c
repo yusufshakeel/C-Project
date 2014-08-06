c7bb80b02e5f46d31b838aa7e1cf4d67e2509b60
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-08-06 15:13:04 +0530

    Revert "info added"
    This reverts commit 3239180351d78f9273aaba70dca1c37361efaa11.

--- codesnippet/io.c
+++ codesnippet/io.c
@@ -1,24 +1,5 @@
-/**
-File: io.c
-Description: This file contains i/o functions to read and write integers
-Author: Yusuf Shakeel
-
-Links
-https://www.facebook.com/yusufshakeel
-https://www.youtube.com/yusufshakeel
-https://plus.google.com/+YusufShakeel
-https://www.twitter.com/yusufshakeel
-https://www.github.com/yusufshakeel
-
-The MIT License (MIT)
-Copyright (c) 2014 Yusuf Shakeel
-*/
-
-
 #define _gc() getchar_unlocked()
 #define _pc(x) putchar_unlocked(x)
-
-//for positive integers only
 
 /* integer */
 inline int getint(){register int x=0,c=_gc();while(!(c>='0'&&c<='9')){c=_gc();}while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}return x;}
@@ -36,6 +17,7 @@ inline void putllint(long long int n){long long int N=n,r=n,c=0;if(N==0){_pc('0'
 typedef long long int iTYPE;
 inline iTYPE getint(){iTYPE x=0,c=_gc();while(!(c>='0'&&c<='9')){c=_gc();}while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}return x;}
 inline void putint(iTYPE n){iTYPE N=n,r=n,c=0;if(N==0){_pc('0');_pc('\n');return;}while(r%10==0){c++;r/=10;}r=0;while(N!=0){r=(r<<3)+(r<<1)+N%10;N/=10;}while(r!=0){_pc(r%10+'0');r/=10;}while(c--){_pc('0');}_pc('\n');}
+
 
 typedef long long int L;
 inline L getint(){L x=0,c=_gc();while(!(c>='0'&&c<='9')){c=_gc();}while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}return x;}
