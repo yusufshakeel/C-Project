742e6120b044c755132994533c1e4f8e3a8bc7b1
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-08-06 15:15:44 +0530

    get signed int added

--- codesnippet/io.c
+++ codesnippet/io.c
@@ -41,3 +41,11 @@ typedef long long int L;
 inline L getint(){L x=0,c=_gc();while(!(c>='0'&&c<='9')){c=_gc();}while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+(c-'0');c=_gc();}return x;}
 inline void putint(L n){L N=n,r=n,c=0;if(N==0){_pc('0');_pc('\n');return;}while(r%10==0){c++;r/=10;}r=0;while(N!=0){r=(r<<3)+(r<<1)+N%10;N/=10;}while(r!=0){_pc(r%10+'0');r/=10;}while(c--){_pc('0');}_pc('\n');}
 
+
+//fast read and write using pointer
+//both positive and negative integer
+//ASCII 45 is '-'
+//ASCII >33 means digits
+typedef long long int L;
+inline void getint(L *x){register char c;int f=0;*x=0;c=_gc();while(c<33){c=_gc();}if(c==45){f++;c=_gc();}while(c>33){*x=(*x<<3)+(*x<<1)+(c-48);c=_gc();}if(f){*x*=-1;}}
+
