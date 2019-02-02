bc82008a50b483b9d0551732e66b358daf399939
bhuvnesh9758 <36470780+bhuvnesh9758@users.noreply.github.com>
2019-02-02 09:02:13 +0530

    Update gcd.c

--- recursion/gcd.c
+++ recursion/gcd.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
-int gcd(int x, int y);
-
+int gcd(int a,int b)
+return (a==0 ? b : gcd(b%a,a);
 int main(){
 	int a, b, g;
 
 	printf("Enter a and b:\n");
 	scanf("%d%d", &a, &b);
+	if(a<0) a=-a;
+	if(b<0) b=-b;
 
 	//gcd
 	g = gcd(a, b);
-
-	//in case g is negative, then convert it into positive
-	if(g < 0){
-		g *= -1;
-	}
-
 	//output
 	printf("GCD(%d, %d) = %d\n", a, b, g);
 	return 0;
-}
-
-int gcd(int x, int y){
-	if(y == 0) return x;
-	else gcd(y, x%y);
 }
