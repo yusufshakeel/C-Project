543e9de2b68f7be3942556079e81d74cdf4e4c82
Creator54 <34543609+Creator54@users.noreply.github.com>
2019-10-29 15:26:22 +0530

    support user input size,elements

--- sorting/quickSort.c
+++ sorting/quickSort.c
@@ -4,35 +4,36 @@
  */
 
 #include <stdio.h>
-#define SIZE 6	//array size
 
-
-void partitionArray(int *a, int beg, int end, int *pivotLoc);
-void quickSort(int *a, int beg, int end);
+void partitionArray(int *j, int beg, int end, int *pivotLoc);
+void quickSort(int *j, int beg, int end);
 
 int main(){
-	int a[SIZE] = {5,2,6,1,3,4};	//unsorted array
-	int i;
-
-	quickSort(a, 0, SIZE-1);	//beg = 0 start index of array end = 5 last index of array
-
+    int k;
+	printf("How many elements?\t");
+	scanf("%d",&k);
+	int i,j[k];
+	for(i=0;i<k;i++){
+	    scanf("%d",&j[i]);
+	}
+	quickSort(j, 0, k-1);	//beg = 0 start index of array end = 5 last index of array
 	//printing sorted element of array
-	for(i = 0; i < SIZE; i++){
-		printf("%d\t", a[i]);
+	for(i = 0; i < k; i++){
+		printf("%d\t", j[i]);
 	}
 	return 0;
 }//main() ends here
 
-void quickSort(int *a, int beg, int end){
+void quickSort(int *j, int beg, int end){
 	int pivotLoc;
 	if(beg < end){
-		partitionArray(a, beg, end, &pivotLoc);	//this will find the pivot location and partition the array
-		quickSort(a, beg, pivotLoc - 1);	//quick sort the left sub array
-		quickSort(a, pivotLoc + 1, end);	//quick sort the right sub array
+		partitionArray(j, beg, end, &pivotLoc);	//this will find the pivot location and partition the array
+		quickSort(j, beg, pivotLoc - 1);	//quick sort the left sub array
+		quickSort(j, pivotLoc + 1, end);	//quick sort the right sub array
 	}
 }//quickSort() ends here
 
-void partitionArray(int *a, int beg, int end, int *pivotLoc){
+void partitionArray(int *j, int beg, int end, int *pivotLoc){
 	int left = beg;		//initially left point to the first element of the array
 	int right = end;	//initially right point to the last element of the array
 	*pivotLoc = left;	//initially pivot point to first element of the array
@@ -41,32 +42,32 @@ void partitionArray(int *a, int beg, int end, int *pivotLoc){
 	while(1){
 
 		//pivot pointing at left
-		while(a[*pivotLoc] <= a[right] && *pivotLoc != right){	//pivot element <= right element
+		while(j[*pivotLoc] <= j[right] && *pivotLoc != right){	//pivot element <= right element
 			right--;	//move right one position towards left
 		}
 
 		if(*pivotLoc == right){	//both left and right pointing at same element of the array
 			break;
-		}else if(a[*pivotLoc] > a[right]){
+		}else if(j[*pivotLoc] > j[right]){
 			//pivot element greater than right element. swap pivot and right element.
-			tmp = a[right];
-			a[right] = a[*pivotLoc];
-			a[*pivotLoc] = tmp;
+			tmp = j[right];
+			j[right] = j[*pivotLoc];
+			j[*pivotLoc] = tmp;
 			*pivotLoc = right;	//pivot is now pointing to right
 		}
 
 		//pivot pointing to right
-		while(a[*pivotLoc] >= a[left] && *pivotLoc != left){	//pivot element >= left element
+		while(j[*pivotLoc] >= j[left] && *pivotLoc != left){	//pivot element >= left element
 			left++;		//move left one position towards right
 		}
 
 		if(*pivotLoc == left){	//both left and right pointing at the same element of the array
 			break;
-		}else if(a[*pivotLoc] < a[left]){
+		}else if(j[*pivotLoc] < j[left]){
 			//pivot element smaller than left element. swap pivot and left element.
-			tmp = a[left];
-			a[left] = a[*pivotLoc];
-			a[*pivotLoc] = tmp;
+			tmp = j[left];
+			j[left] = j[*pivotLoc];
+			j[*pivotLoc] = tmp;
 			*pivotLoc = left;	//pivot is now pointing to left
 		}
 	}
